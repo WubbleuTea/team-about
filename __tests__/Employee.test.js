0e313d9cc2a81001ae01c13d363f96af1433f897
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-22 16:17:20 -0500

    added package.json and started Employee tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee');
+
+test('creates and asks employee questions', () => {
+    const employee = new Employee('Jake', '87309fhu', 'asdfasdf@yahoo.com');
+
+    expect(employee.name).toBe('Jake');
+    expect(employee.id).toEqual(expect.any(String))
+    expect(employee.email).toEqual(expect.any(String))
+})
