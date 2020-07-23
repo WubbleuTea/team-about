209335f7895bf9d0ac5d7b23aac03241217d010b
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-23 18:44:00 -0500

    updated questions

--- utils/questions.js
+++ utils/questions.js
@@ -1,15 +1,15 @@
 const inquirer = require('inquirer');
 
-const employeeQuestions = [
+const managerQuestions = [
     {
         type: 'input',
         name: 'name',
-        message: "Enter this employee's name.",
+        message: "Enter the manager's name.",
         validate: nameInput => {
             if (nameInput) {
                 return true;
             } else {
-                console.log("Please enter your employee's name!");
+                console.log("Please enter manager's name!");
                 return false;
             }
         }
@@ -17,12 +17,12 @@ const employeeQuestions = [
     {
         type: 'input',
         name: 'id',
-        message: "Enter this employee's id.",
+        message: "Enter this manager's id.",
         validate: idInput => {
             if (idInput) {
                 return true;
             } else {
-                console.log("Please enter your employee's id!");
+                console.log("Please enter your manager's id!");
                 return false;
             }
         }
@@ -30,44 +30,25 @@ const employeeQuestions = [
     {
         type: 'input',
         name: 'email',
-        message: "Enter this employee's e-mail address.",
+        message: "Enter this manager's e-mail address.",
         validate: emailInput => {
             if (emailInput) {
                 return true;
             } else {
-                console.log("Please enter your employee's e-mail!");
+                console.log("Please enter your manager's e-mail!");
                 return false;
             }
         }
-    }
-];
-
-const internQuestions = [
-    {
-        type: 'input',
-        name: 'school',
-        message: "Enter this employee's school.",
-        validate: schoolInput => {
-            if (schoolInput) {
-                return true;
-            } else {
-                console.log("Please enter your employee's school!");
-                return false;
-            }
-        }
-    }
-]
-
-const managerQuestions = [
+    },
     {
         type: 'input',
         name: 'officeNumber',
-        message: "Enter this employee's office number.",
+        message: "Enter this manager's office number.",
         validate: numberInput => {
             if (numberInput) {
                 return true;
             } else {
-                console.log("Please enter your employee's school!");
+                console.log("Please enter your manager's office number!");
                 return false;
             }
         }
@@ -77,13 +58,107 @@ const managerQuestions = [
 const engineerQuestions = [
     {
         type: 'input',
+        name: 'name',
+        message: "Enter this engineer's name.",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your engineer's name!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter this engineer's id.",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log("Please enter your engineer's id!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter this engineer's e-mail address.",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter your engineer's e-mail!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
         name: 'github',
-        message: "Enter this employee's GitHub username.",
+        message: "Enter this engineer's GitHub username.",
         validate: githubInput => {
             if (githubInput) {
                 return true;
             } else {
-                console.log("Please enter your employee's GitHub username!");
+                console.log("Please enter your engineer's GitHub username!");
+                return false;
+            }
+        }
+    }
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter this intern's name.",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your intern's name!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter this intern's id.",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log("Please enter your intern's id!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter this intern's e-mail address.",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter your intern's e-mail!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "Enter this intern's school.",
+        validate: schoolInput => {
+            if (schoolInput) {
+                return true;
+            } else {
+                console.log("Please enter your intern's school!");
                 return false;
             }
         }
