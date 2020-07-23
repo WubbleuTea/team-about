bedd8b27fed57dece0ac432edd4c008437c24014
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-23 16:12:28 -0500

    employee functions and info now pass tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,19 @@
 const Employee = require('../lib/Employee');
 
 test('creates and asks employee questions', () => {
-    const employee = new Employee('Jake', '87309fhu', 'asdfasdf@yahoo.com');
+    const employee = new Employee('Jake', '010101', 'Jake@gmail.com');
 
     expect(employee.name).toBe('Jake');
     expect(employee.id).toEqual(expect.any(String))
-    expect(employee.email).toEqual(expect.stringContaining('@'))
-})
+    expect(employee.email).toEqual(expect.any(String))
+});
+
+
+test('returns Employee information', () => {
+    const employee = new Employee('Jake', '87309fhu', 'Jake@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining('Jake'));
+    expect(employee.getId()).toEqual(expect.any(String))
+    expect(employee.getEmail()).toEqual(expect.stringContaining('@'))
+    expect(employee.getRole()).toEqual(expect.stringMatching('Employee'))
+});
