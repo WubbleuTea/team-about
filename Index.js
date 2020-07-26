071b0361ee31fb9da2121f64787d8341e7961966
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-26 14:11:40 -0500

    updated HTML and moved writefile to Index

--- Index.js
+++ Index.js
@@ -1,9 +1,10 @@
 let employeeArr = [];
+const fs = require('fs')
 const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const { generateHTML } = require('./utils/generateHTML')
+// const { generateHTML } = require('./utils/generateHTML')
 const { managerQuestions, internQuestions, engineerQuestions } = require('./utils/questions')
 
 const managerInfo = () => {
@@ -72,6 +73,66 @@ const generateOrNot = (anotherMember) => {
     }
 }
 
+const generateHTML = () => {
+    fs.writeFile('./dist/index.html', writeHTML(), (err) => {
+        if (err) throw err;
+        console.log('The file has been created!');
+      }); 
+}
+
+const writeHTML = () => {
+    console.log(`HTML being written`);
+    console.log(employeeArr)
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About the Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar sticky-top navbar-light justify-content-center" style="background-color: #e3f2fd;">
+        <h1 class="navbar-text">Our Team</h1>
+    </nav>
+    <div class="container d-flex flex-row p-4">
+        ${writeCards()}
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>`
+
+}
+
+
+const writeCards = () =>{
+    console.log(`made it into WriteCards`)
+    let html = ``;
+    //If you want you can write seperate functions for whether it is a Manager, Intern, etc...or you can do `if (employee instanceof Intern){blah blah}`
+    employeeArr.map(employee => {
+        const { name, role, id, extraInfo, email } = employee
+        html +=
+         `<div class="card" style="width: 18rem;">
+            <div class="card-header bg-success">
+                ${name}
+                ${role}
+            </div>
+            <div class="card-body">
+                ${id}
+                ${extraInfo}
+                ${email}
+            </div>
+        </div>`
+    });    
+    console.log(`I am about to return`)
+    return html;
+}
+
+
 managerInfo();
 
-module.exports = [ employeeArr ]
+// module.exports = [ employeeArr ]
